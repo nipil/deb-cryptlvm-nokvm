68c6a54384e0708014349ac35ec726e357a33f28
nipil <nipil@users.noreply.github.com>
2014-11-30 11:18:18 +0100

    usage, config, prereq

--- setup.sh
+++ setup.sh
@@ -21,29 +21,92 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
 ###############################################################################
 
+########################################################################################
 # load configuration file
+#
 source template.conf
 
-# debug
-echo "$DCN_DISK"
-echo "$DCN_FSTYPE"
-echo "$DCN_VG_NAME"
-echo "$DCN_ROOTFS_SIZE"
-echo "$DCN_SWAP_SIZE"
-for i in ${DCN_PARTITIONS[@]}; do
-	echo $i
+########################################################################################
+# show usage
+#
+
+usage()
+{
+	echo -ne "Usage: ${0} operation [args]\n" \
+	  "\tconfig\n" \
+	  "\tprereq\n" \
+	  "\tusage\n"
+
+}
+
+########################################################################################
+# dump loaded configuration
+#
+
+config()
+{
+
+DCN="DCN_DISK DCN_FSTYPE DCN_VG_NAME DCN_ROOTFS_SIZE DCN_SWAP_SIZE \
+     DCN_BOOTSTRAP_MIRROR DCN_BOOTSTRAP_ARCH DCN_BOOTSTRAP_RELEASE \
+     DCN_INSTALL_RECOMMENDS DCN_INSTALL_ETCKEEPER DCN_HOST_NAME \
+     DCN_HOST_IFACE DCN_HOST_IP DCN_HOST_MASK DCN_HOST_GW \
+     DCN_SSH_PORT_PREBOOT DCN_SSH_PORT_BOOTED DCN_SSH_AUTHKEYS_SRCFILE"
+
+for i in ${DCN}; do
+	echo "${i}=${!i}"
 done
-echo "$DCN_BOOTSTRAP_MIRROR"
-echo "$DCN_BOOTSTRAP_ARCH"
-echo "$DCN_BOOTSTRAP_RELEASE"
-echo "$DCN_INSTALL_RECOMMENDS"
-echo "$DCN_INSTALL_ETCKEEPER"
-echo "$DCN_HOST_NAME"
-echo "$DCN_HOST_IFACE"
-echo "$DCN_HOST_IP"
-echo "$DCN_HOST_MASK"
-echo "$DCN_HOST_GW"
-echo "$DCN_SSH_PORT_PREBOOT"
-echo "$DCN_SSH_PORT_BOOTED"
-echo "$DCN_SSH_AUTHKEYS_SRCFILE"
+
+echo "DCN_PARTITIONS=("
+for i in ${DCN_PARTITIONS[@]}; do
+	echo ${i}
+done
+echo ")"
+
+}
+
+########################################################################################
+# verify that the required tools are available
+#
+
+prereq()
+{
+
+PREREQ="fdisk mkfs.ext4 cryptsetup pvcreate vgcreate lvcreate mkswap \
+        mkdir mount swapon chmod grep awk debootstrap chroot umount swapoff \
+        vgchange"
+
+for i in ${PREREQ}; do
+	j=`which ${i}`
+	if [[ ! -x ${j} ]]; then
+		echo "${i} not found"
+		exit 1
+	else
+		echo "found ${i} at ${j}"
+	fi
+done
+
+}
+
+########################################################################################
+# main part
+#
+
+case "${1}" in
+	usage)
+		usage
+		exit 0
+		;;
+	config)
+		config
+		exit 0
+		;;
+	prereq)
+		prereq
+		exit 0
+		;;
+	*)
+		usage
+		exit 0
+		;;
+esac
 
